104ce8a437b662378b10145cfc61b86848b05bc6
nwudochika <nwudochika@gmail.com>
2026-02-21 11:20:56 -0330

    adding extra configurations

--- main.tf
+++ main.tf
@@ -164,3 +164,163 @@ resource "aws_route_table_association" "a2" {
   subnet_id      = aws_subnet.public2.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+# ALB Security Group
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.main.id
+  name        = "alb-sg"
+  description = "Security group for public ALB"
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Public allowed
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 security group
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main.id
+  name        = "ec2-sg"
+  description = "Security group for webserver"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ALB Target group
+resource "aws_lb_target_group" "tg" {
+  name        = "tf-lb-tg"
+  target_type = "instance"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+}
+
+# Load Balancer
+resource "aws_lb" "alb" {
+  name               = "lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
+  enable_deletion_protection = false
+}
+
+# HTTPS Listener
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08" 
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn 
+  }
+}
+
+# Route Domain to ALB
+resource "aws_route53_record" "www" {
+    zone_id = var.zone_id
+    name    = "www.fcjnwudo.com" 
+    type    = "A"
+
+    alias {
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
+    evaluate_target_health = true
+    }
+}
+
+# Data Source AMI
+data "aws_ami" "amazon_linux_latest" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+# Launch Template
+resource "aws_launch_template" "ec2_launchtemplate" {
+  name_prefix   = "ec2-launchtemplate"
+  image_id      = data.aws_ami.amazon_linux_latest.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  user_data = base64encode(<<EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Welcome to Fidelis Nwudo web server deployed with Terraform!</h1>" > /var/www/html/index.html
+EOF
+)
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ASG-Web-Server"
+    }
+  }
+}
+
+# Autoscaling Group
+resource "aws_autoscaling_group" "asg" {
+  name               = "asg-terraform"
+  max_size           = 3
+  min_size           = 2
+  desired_capacity   = 2
+  target_group_arns = [aws_lb_target_group.tg.arn]
+
+
+  vpc_zone_identifier = [
+    aws_subnet.public1.id,
+    aws_subnet.public2.id
+  ]
+
+  launch_template {
+    id      = aws_launch_template.ec2_launchtemplate.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "ASG-Instances"
+    propagate_at_launch = true
+  }
+}
